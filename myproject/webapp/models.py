38f33f325bb7a48f6ea5868366c7152bf9ee8d73
Fuad <fuadhasanemon8@gmail.com>
2020-07-13 04:46:06 +0600

    second commit

--- myproject/webapp/models.py
+++ myproject/webapp/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class employees(models.Model):
-    firstname=models.Cha
+    firstname=models.CharField(max_length=10)
+    last_name=models.CharField(max_length=10)
+    emp_id=models.IntegerField()
+
+    def __str__(self):
+        return self.firstname
